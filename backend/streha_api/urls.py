c67094019801515f5a79a889a98c015cb8598e28
Bekim19800 <154931122+Bekim19800@users.noreply.github.com>
2025-04-06 00:20:42 +0200

    Update urls.py

--- backend/streha_api/urls.py
+++ backend/streha_api/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('listings/', views.listings, name='listings'),
+
+    # Optional fix:
+    path('', views.index),  # Add this only if you have views.index
 ]
