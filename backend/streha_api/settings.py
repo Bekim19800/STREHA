507fc8e14eba4b92a29237c5e428556985da5c8b
Bekim19800 <154931122+Bekim19800@users.noreply.github.com>
2025-04-05 22:31:04 +0200

    Update settings.py

--- backend/streha_api/settings.py
+++ backend/streha_api/settings.py
@@ -1,2 +1,16 @@
+INSTALLED_APPS = [
+    'corsheaders',
+    'django.contrib.staticfiles',
+    ...
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    ...
+]
+
+CORS_ALLOW_ALL_ORIGINS = True  # 👈 allows any frontend to connect
+
 ALLOWED_HOSTS = ['your-backend-name.onrender.com']
 ROOT_URLCONF = 'streha_api.urls'
