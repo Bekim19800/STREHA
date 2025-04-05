8bcb2a3af73144a046fd8839db4df81caeb4843e
Bekim19800 <154931122+Bekim19800@users.noreply.github.com>
2025-04-05 23:03:43 +0200

    Update settings.py

--- backend/streha_api/settings.py
+++ backend/streha_api/settings.py
@@ -1,16 +1,27 @@
+ALLOWED_HOSTS = ['streha.onrender.com']
+
+ROOT_URLCONF = 'streha_api.urls'
+
 INSTALLED_APPS = [
     'corsheaders',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
-    ...
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    ...
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True  # 👈 allows any frontend to connect
-
-ALLOWED_HOSTS = ['your-backend-name.onrender.com']
-ROOT_URLCONF = 'streha_api.urls'
+# Enable CORS so React frontend can access API
+CORS_ALLOW_ALL_ORIGINS = True
